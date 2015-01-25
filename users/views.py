985aa65ad30c384008079284ee2bfac3740e6139
Evgeny <tumanovevgeniy@yandex.ru>
2015-01-26 00:42:09 +0300

    Working

--- users/views.py
+++ users/views.py
@@ -72,6 +72,7 @@ def user_detail(request, vk_id):
         return HttpResponse(status=204)
 
 def sendNotification (user_vk_id):
+    MESSAGE_TEXT = "'Вернись! Я все прощу!'"
     ID_OF_VK_APP = '4737414' # aka client_id
     SECRET_KEY_OF_VK_APP = '5DQcPsFP2bMbSwbkTKNW' # aka client_secret
     url_to_get_access_token = 'https://oauth.vk.com/access_token?client_id=' + ID_OF_VK_APP + '&client_secret=' + SECRET_KEY_OF_VK_APP + '&v=5.27&grant_type=client_credentials'
@@ -80,7 +81,7 @@ def sendNotification (user_vk_id):
     current_access_token = json_with_access_token['access_token']
     print(current_access_token)
     url_to_send_notification = 'https://api.vk.com/method/secure.sendNotification?user_id=' + \
-                               user_vk_id + '&message=' + 'Test' + '&v=5.27&client_secret=' + \
+                               user_vk_id + '&message=' + MESSAGE_TEXT + '&v=5.27&client_secret=' + \
                                SECRET_KEY_OF_VK_APP + '&access_token=' + current_access_token
     response = urllib2.urlopen(url_to_send_notification)
     json_notification = json.load(response)
