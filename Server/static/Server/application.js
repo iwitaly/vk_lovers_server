64b45305032f9383569c05809b9ad0fa2b02fe3b
Evgeny <tumanovevgeniy@yandex.ru>
2015-01-30 17:44:38 +0300

    Working on design

--- Server/static/Server/application.js
+++ Server/static/Server/application.js
@@ -42,10 +42,10 @@ function addRowToTableWithFriends (viewerUserIdNumber, cellUserIdNumber, photo, 
     var valueDateString = "'" + toWhoVkIdString + "'" ;
     var valueSexString = "'" + toWhoVkIdString + "'" ;
     $('#main-table').append("<tr class='item'>" +
-        '<td>' + "<img src='" + photo + "'>" + '</td>' +
-        "<td class='name-field text-center'>" + first_name + ' ' + last_name + '</td>' +
-        '<td>' + "<button class='button-date' id=" + idDateString + ' value=' + valueDateString + '>' + 'Date'+ '</button>' + '</td>' +
-        '<td>' + "<button class='button-sex' id=" + idSexString + ' value=' +  valueSexString + '>'  + 'Sex'+ '</button>' + '</td>' +
+        "<td class='vert-align'>" + "<img src='" + photo + "'>" + '</td>' +
+        "<td class='name-field vert-align'>" + first_name + ' ' + last_name + '</td>' +
+        "<td class='vert-align'>" + "<button class='button-date' id=" + idDateString + ' value=' + valueDateString + '>' + 'Date'+ '</button>' + '</td>' +
+        "<td class='vert-align'>" + "<button class='button-sex' id=" + idSexString + ' value=' +  valueSexString + '>'  + 'Sex'+ '</button>' + '</td>' +
     '</tr>');
 }
 
